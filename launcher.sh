4cbf55938ab525ccda92f9eb3cb6b4231dfb4f1c
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-01-22 10:16:12 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 cd /
-cd /home/pi/raspberry-pi-face-detection #the filepath to where the detector.py file lies
+cd /home/pi/raspberry-pi-face-detection-master #the filepath to where the detector.py file lies
 sudo modprobe bcm2835-v4l2
 DISPLAY=:0 sudo python detector.py
 cd /
